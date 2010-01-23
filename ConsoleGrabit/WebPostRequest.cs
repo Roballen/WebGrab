e2861339df02a4835ba71da28237abc00758bb07
Robert Allen <tellutwurp@gmail.com>
2010-01-22 17:59:15 -0700

    initial commit

---
+++ ConsoleGrabit/WebPostRequest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+
+namespace ConsoleGrabit
+{
+    public class WebPostRequest
+    {
+        private WebRequest _request;
+        private HttpWebResponse _response;
+        private Collection<string> _querydata;
+
+        public WebPostRequest(string url)
+        {
+            _request = WebRequest.Create(url);
+            _request.Method = "POST";
+            _querydata = new Collection<string>();
+        }
+
+        public void Add(string key, string value)
+        {
+            _querydata.Add(String.Format("{0}={1}",key,HttpUtility.UrlEncode(value)));
+        }
+
+        public string GetResponse()
+        {
+            _request.ContentType = "application/x-www-form-urlencoded";
+            var parameters = String.Join("&",  _querydata.ToArray());
+            _request.ContentLength = parameters.Length;
+
+            var sw = new StreamWriter(_request.GetRequestStream());
+            sw.Write(parameters);
+            sw.Close();
+
+            _response = (HttpWebResponse) _request.GetResponse();
+            var sr = new StreamReader(_response.GetResponseStream());
+            return sr.ReadToEnd();
+
+        }
+
+    }
+}
---
+++ GrabIt/ConsoleGrabit/Lead.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleGrabit
+{
+    public class Lead
+    {
+
+        public Lead()
+        {
+            _city = "";
+            _zip = "";
+            _streetaddress = "";
+            _state = "";
+            _recordeddate = "";
+            _middle = "";
+            _last = "";
+            _first = "";
+            _debt = "";
+        }
+
+        #region get/set
+
+        public string First
+        {
+            get { return _first; }
+            set { _first = value; }
+        }
+
+        public string Last
+        {
+            get { return _last; }
+            set { _last = value; }
+        }
+
+        public string Middle
+        {
+            get { return _middle; }
+            set { _middle = value; }
+        }
+
+        public string Businessname
+        {
+            get { return _businessname; }
+            set { _businessname = value; }
+        }
+
+        public string Streetaddress
+        {
+            get { return _streetaddress; }
+            set { _streetaddress = value; }
+        }
+
+        public string City
+        {
+            get { return _city; }
+            set { _city = value; }
+        }
+
+        public string State
+        {
+            get { return _state; }
+            set { _state = value; }
+        }
+
+        public string Zip
+        {
+            get { return _zip; }
+            set { _zip = value; }
+        }
+
+        public string Debt
+        {
+            get { return _debt; }
+            set { _debt = value; }
+        }
+
+        public string Recordeddate
+        {
+            get { return _recordeddate; }
+            set { _recordeddate = value; }
+        }
+
+        #endregion
+
+
+        #region members
+
+        private string _first;
+        private string _last;
+        private string _middle;
+        private string _businessname;
+        private string _streetaddress;
+        private string _city;
+        private string _state;
+        private string _zip;
+        private string _debt;
+        private string _recordeddate;
+
+        #endregion
+
+    }
+}
